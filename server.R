8e07693ab72d228ae809dbda27ae692076d7a618
yfwang <goodyufu@gmail.com>
2017-09-04 14:19:46 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,33 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(datasets)
+
+# We tweak the "am" field to have nicer factor labels. Since this doesn't
+# rely on any user inputs we can do this once at startup and then use the
+# value throughout the lifetime of the application
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData)
+  })
+})
