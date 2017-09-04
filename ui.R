fc2db3692607a284d32b6b7b8cf2592dee406f60
yfwang <goodyufu@gmail.com>
2017-09-04 14:20:02 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,28 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Miles Per Gallon (MPG)"),
+  
+  sidebarPanel(
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear"))
+  ),
+  
+  mainPanel(
+    h3(textOutput("caption")),
+    
+    plotOutput("mpgPlot")
+  )
+))
